1a9fec48ee19955395f9f202c92b2f1ae8b7f5aa
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-01-07 18:44:58 +0100

    fixed

---
+++ SET_UP.php
@@ -0,0 +1,8 @@
+<?php  
+
+$MODE = "DEV";
+$APP = [
+  "APP_NAME" => "Beko Federal Credit Union (BEKOFCU)",
+  "APP_LOGO" => $_SERVER['HTTP_HOST'] . $MODE == "DEV" ? "/swiss-main/logg.png" : "logo.png",
+  "APP_LINK" => "https://bekofcu.com/"
+];
