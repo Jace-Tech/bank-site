bb98c5701dffd267e0d03f94c79529748893404a
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-01-23 03:59:19 +0100

    styled forms

--- admin/generate.php
+++ admin/generate.php
@@ -4,7 +4,7 @@ require_once 'inc/header.php';
 
 $USERS = mysqli_fetch_all(returnQuery("SELECT * FROM users"), MYSQLI_ASSOC);
 
-if(isset($_POST['generate'])) {
+if (isset($_POST['generate'])) {
   $user = $_POST['user'];
   $start = $_POST['startdate'];
   $end = $_POST['enddate'];
@@ -21,32 +21,42 @@ if(isset($_POST['generate'])) {
     <!-- Quick Overview -->
     <div class="row row-deck">
       <div class="col-12">
-        <form action="">
-          <div class="form-group">
-            <label for="user">User</label>
-            <select name="user" class="form-control" id="user">
-              <option value="" selected disabled>Select User</option>
-              <?php foreach($USERS AS $user): ?>
-                <option value="<?= $user['id']; ?>">
-                  <?= $user['fullname']; ?>
-                </option>
-              <?php endforeach; ?>
-            </select>
+        <form action="" class="w-100">
+          <div class="row">
+            <div class="col-12">
+              <div class="form-group">
+                <label for="user">User</label>
+                <select name="user" class="form-control" id="user">
+                  <option value="" selected disabled>Select User</option>
+                  <?php foreach ($USERS as $user) : ?>
+                    <option value="<?= $user['id']; ?>">
+                      <?= $user['fullname']; ?>
+                    </option>
+                  <?php endforeach; ?>
+                </select>
+              </div>
+            </div>
+
+            <div class="col-sm-12 col-md-6">
+              <div class="form-group">
+                <label for="startdate">Start Date</label>
+                <input type="date" class="form-control" name="startdate" id="startdate">
+              </div>
+            </div>
+
+            <div class="col-sm-12 col-md-6">
+              <div class="form-group">
+                <label for="enddate">End Date</label>
+                <input type="date" class="form-control" name="enddate" id="enddate">
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="mt-3">
+                <button type="submit" name="generate" class="btn btn-primary">Generate History</button>
+              </div>
+            </div>
           </div>
 
-          <div class="form-group">
-            <label for="startdate">Start Date</label>
-            <input type="date" class="form-control" name="startdate" id="startdate">
-          </div>
-
-          <div class="form-group">
-            <label for="enddate">End Date</label>
-            <input type="date" class="form-control" name="enddate" id="enddate">
-          </div>
-
-          <div class="mt-3">
-            <button type="submit" name="generate" class="btn btn-primary">Generate History</button>
-          </div>
         </form>
       </div>
     </div>
