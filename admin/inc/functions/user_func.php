4e4b15d254fba9a088d89e271fff529b1a1915d6
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-02-05 06:16:09 +0100

    updated

--- admin/inc/functions/user_func.php
+++ admin/inc/functions/user_func.php
@@ -857,8 +857,6 @@ function updateProfileImage($file, $user_id) {
 
     if (!empty($file['img'])) {
         $profileImage =  time(). sanitize($file['img']['name']);
-
-        print_r($file);
         $profileImageTmp = $file['img']['tmp_name'];
         move_uploaded_file($profileImageTmp, "../media/users/$profileImage");
     } else {
