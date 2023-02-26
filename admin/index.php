3a45218cafcd919417e7e88726fc9a16f779ddc7
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-02-26 22:17:43 +0100

    thinkering

--- admin/index.php
+++ admin/index.php
@@ -86,7 +86,7 @@ require_once 'inc/header.php';
     
                                         <tr>
                                             <td class="font-w600 text-center" style="width: 100px;">
-                                                <a href="#"><?=  generateTransactionId($id); ?></a>
+                                                <a href="#" style="white-space: nowrap;"><?=  generateTransactionId($id); ?></a>
                                             </td>
                                             <td class="d-none d-sm-table-cell">
                                                 <a href="#" style="white-space: nowrap; text-overflow: ellipsis;"><?= $user ? $user['fullname'] : "<i>Nill</i>"; ?></a>
@@ -94,7 +94,7 @@ require_once 'inc/header.php';
                                             <td>
                                                 <span style="white-space: nowrap; text-overflow: ellipsis;"><?= $description ? sub_word($description, 3) : "<i>No description</i>"; ?></span>
                                             </td>
-                                            <td class="font-w600 text-right <?= $class ?>">$<?= number_format($amount); ?></td>
+                                            <td class="font-w600 text-right">$<?= number_format($amount); ?></td>
                                             <td class="font-w600 text-right"><a href="backdate" style="white-space: nowrap;"><strong><?= date("M d, Y - h:i", strtotime($created_at)); ?></a></td>
                                             <td class="font-w600 text-right"><a href="backdate?id=<?= $id; ?>" style="white-space: nowrap; text-overflow: ellipsis;" class="shadow btn btn-sm btn-primary">Backdate Transaction</a></td>
                                         </tr>
