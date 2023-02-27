adbf20a28931b8d2ddd12a9c77950b381dae20fe
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-02-27 15:04:17 +0100

    updated

--- admin/handler/delete.php
+++ admin/handler/delete.php
@@ -9,9 +9,9 @@ if (isset($_GET['delete'])) {
   $response = returnQuery("DELETE FROM transactions WHERE id = '$del_id'");
   if ($response) {
     $_SESSION['A_ALERT'] = json_encode(['type' => "success", 'message' => "Transaction deleted successfully"]);
-    header("Location: ../all-transaction");
+    header("Location:" . $_SERVER["HTTP_REFERER"]);
   } else {
     $_SESSION['A_ALERT'] = json_encode(['type' => "error", 'message' => "Failed to delete transaction"]);
-    header("Location: ../all-transaction");
+    header("Location:" . $_SERVER["HTTP_REFERER"]);
   }
 }
