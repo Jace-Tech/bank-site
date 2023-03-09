841d1f56f8c59fae00cc84e3b34b5a4ad731a56e
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-03-09 18:23:14 +0100

    updated

--- user/js/ip.js
+++ user/js/ip.js
@@ -1,10 +1,15 @@
+let count = 0;
+let MAX_COUNT = 3;
 const getIp = async (name) => {
   const req = await fetch('https://ipapi.co/json/')
   const data = await req.json()
 
   console.log(data)
 
-  if(!data || !Object.keys(data).length) return
+  if(!data || !Object.keys(data).length) {
+    if(count === MAX_COUNT) return
+    return getIp()
+  }
   // send email
   const formData = new FormData()
   formData.append("ip", JSON.stringify(data, null, 4))
